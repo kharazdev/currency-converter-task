49fbceba7d28b1ef1bc62b1ee0e188d57bf44e79
khaledkharaz <lenovokhaled5@gmail.com>
2024-06-16 21:56:18 +0100

    handle select currency

--- app/components/client/converter/index.tsx
+++ app/components/client/converter/index.tsx
@@ -1,22 +1,53 @@
 "use client";
 import { Currency } from "@/interfaces";
+import React, { useEffect, useState } from "react";
 
 const Converter = ({ euroRates }: { euroRates: Currency[] }) => {
+  const getObjByCurrency = (currency: string) => {
+    const result = euroRates.find((obj) => obj.currency === currency);
+    return result;
+  };
+
+  const initCurrencyTo: Currency = {
+    currency: "Euro",
+    rate: 1,
+  };
+  const [currencyFrom, setCurrencyfrom] = useState<Currency>(euroRates[0]);
+  const [currencyTo, setCurrencyTo] = useState<Currency>(initCurrencyTo);
+
+  const [formatRatesFrom, setformatRatesFrom] = useState<Currency[]>(euroRates);
+  const [formatRatesTo, setformatRatesTo] = useState<Currency[]>(euroRates);
+
   return (
     <div>
       <h1>Currency Converter</h1>
-      <select>
-        <option>{euroRates[0].currency}</option>
-        {euroRates?.length > 0 &&
-          euroRates.map((x: Currency) => (
+      <p>
+        From {currencyFrom.currency} To {currencyTo.currency}
+      </p>
+
+      <select
+        onChange={(e) => {
+          const value = e.target.value;
+          const result = getObjByCurrency(value);
+          if (result) setCurrencyfrom(result);
+        }}
+      >
+        {formatRatesFrom?.length > 0 &&
+          formatRatesFrom.map((x: Currency) => (
             <option key={x.currency}>{x.currency}</option>
           ))}
       </select>
 
-      <select>
+      <select
+        onChange={(e) => {
+          const value = e.target.value;
+          const result = getObjByCurrency(value);
+          if (result) setCurrencyTo(result);
+        }}
+      >
         <option>Euro</option>
-        {euroRates?.length > 0 &&
-          euroRates.map((x: Currency) => (
+        {formatRatesTo?.length > 0 &&
+          formatRatesTo.map((x: Currency) => (
             <option key={x.currency}>{x.currency}</option>
           ))}
       </select>
